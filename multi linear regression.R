d7076ac3804021ff621d6abf702ff5217aaa04bf
Sanjeet19 <32523424+Sanjeet19@users.noreply.github.com>
2018-08-13 20:21:38 +0530

    Add files via upload

---
+++ multi linear regression.R
@@ -0,0 +1,26 @@
+a=readline()
+datas = read.csv(a)
+# View(datas)
+plot(datas, main='pair wise scatter plot')
+
+l=lm(Price~.,data=datas);
+round(cor(datas),3)
+
+datac = datas
+a=l
+
+repeat{
+  View(colnames(datac))
+  print(summary(a))
+  z <- readline()
+  z <- as.integer(z)
+  if(z==0){
+    break
+  } else{
+    datac=datac[,-z]
+    a=lm(Price~.,data=datac)
+
+  }
+
+}
+plot(a)
